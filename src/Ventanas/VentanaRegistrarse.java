8a621864537ee416df653712e5e55de2331d4c78
GemaRubio <80826145+GemaRubio@users.noreply.github.com>
2022-12-27 10:42:22 +0100

    Update VentanaRegistrarse.java

--- src/Ventanas/VentanaRegistrarse.java
+++ src/Ventanas/VentanaRegistrarse.java
@@ -3,8 +3,22 @@ package Ventanas;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 import javax.swing.*;
+
+import Data.Instrumento;
+import Data.Usuario;
 
 public class VentanaRegistrarse  extends JFrame{
 
@@ -137,8 +151,6 @@ public class VentanaRegistrarse  extends JFrame{
 					ex.printStackTrace();
 				}
 			}
-			}
-			
 		});
 
 		//Boton Inicio
