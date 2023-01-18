ac81d0981f2bd534bf12397ec6a427a6f0d20a27
GemaRubio <80826145+GemaRubio@users.noreply.github.com>
2023-01-18 19:58:03 +0100

    Update VentanaAdmin.java

--- src/Ventanas/VentanaAdmin.java
+++ src/Ventanas/VentanaAdmin.java
@@ -1,26 +1,31 @@
 package Ventanas;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+
+import Data.Usuario;
 
 public class VentanaAdmin extends JFrame{
 	
-	public VentanaAdmin() {
+	public VentanaAdmin(Usuario u) {
 		
-		// Configuración de la ventana.
+		// ConfiguraciÃ³n de la ventana.
 		setTitle("Ventana administrador");
 		setSize(750, 300);
 		setLocation(450, 250);;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLayout(new BorderLayout());
 		
-		// Creación de contenedores de la ventana y su configuración.
+		// CreaciÃ³n de contenedores de la ventana y su configuraciÃ³n.
 		JPanel pNorte = new JPanel();
 		pNorte.setLayout(new GridLayout(1,7));
 		JPanel pCentro = new JPanel();
 		pCentro.setLayout(new GridLayout(1,4));
 		
-		// Creación de componentes de la ventana y su configuración.
+		// CreaciÃ³n de componentes de la ventana y su configuraciÃ³n.
 		JButton btnInicio = new JButton("Inicio");
 		JPanel pNombreTienda = new JPanel();
 		JPanel pPanelVacio1 = new JPanel();
@@ -29,11 +34,11 @@ public class VentanaAdmin extends JFrame{
 		JPanel pPanelVacio4 = new JPanel();
 		
 		JButton btnComprar = new JButton("Comprar");
-		JButton btnCodigoFuente = new JButton("Código fuente");
+		JButton btnCodigoFuente = new JButton("CÃ³digo fuente");
 		JButton btnVerBaseDatos = new JButton("Ver base de datos");
 		JButton btnVerFicheros = new JButton("Ver ficheros");
 		
-		// Asignación de los componentes a los contenedores.
+		// AsignaciÃ³n de los componentes a los contenedores.
 		pNorte.add(btnInicio);
 		pNorte.add(pNombreTienda);
 		pNorte.add(pPanelVacio1);
@@ -48,11 +53,25 @@ public class VentanaAdmin extends JFrame{
 		add(pNorte, BorderLayout.NORTH);
 		add(pCentro, BorderLayout.CENTER);
 		
-		// Configuración de la ventana.
+		// ConfiguraciÃ³n de la ventana.
 		setVisible(true);
 		
-		// Configuración de los eventos.
+		// ConfiguraciÃ³n de los eventos.
+		btnComprar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				VentanaUsuario vp = new VentanaUsuario(u);
+			}
+		});
 		
+		btnInicio.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				VentanaUsuario vp = new VentanaUsuario(u);
+			}
+		});
 	}
 
 }
