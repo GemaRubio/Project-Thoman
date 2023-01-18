ce45daea962a3b1ed6b63774cb9cc6641bd1f1aa
GemaRubio <80826145+GemaRubio@users.noreply.github.com>
2023-01-18 19:51:29 +0100

    Update VentanaCambiarContrasenya.java

--- src/Ventanas/VentanaCambiarContrasenya.java
+++ src/Ventanas/VentanaCambiarContrasenya.java
@@ -1,20 +1,25 @@
 package Ventanas;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+
+import Data.Usuario;
 
 public class VentanaCambiarContrasenya extends JFrame {
 	
-	public VentanaCambiarContrasenya() {
+	public VentanaCambiarContrasenya(Usuario u) {
 		
-		// Configuración de la ventana.
-		setTitle("Ventana cambiar contraseña");
+		// ConfiguraciÃ³n de la ventana.
+		setTitle("Ventana cambiar contraseÃ±a");
 		setSize(750, 300);
 		setLocation(450, 250);;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLayout(new BorderLayout());
 		
-		// Creación de contenedores de la ventana y su configuración.
+		// CreaciÃ³n de contenedores de la ventana y su configuraciÃ³n.
 		JPanel pNorte = new JPanel();
 		pNorte.setLayout(new GridLayout(1,7));
 		JPanel pCentro = new JPanel();
@@ -22,31 +27,31 @@ public class VentanaCambiarContrasenya extends JFrame {
 		JPanel pSur = new JPanel();
 		pSur.setLayout(new FlowLayout());
 		
-		// Creación de componentes de la ventana y su configuración.
+		// CreaciÃ³n de componentes de la ventana y su configuraciÃ³n.
 		JButton btnInicio = new JButton("Inicio");
 		JPanel pNombreTienda = new JPanel();
 		JPanel pPanelVacio1 = new JPanel();
 		JPanel pPanelVacio2 = new JPanel();
 		JPanel pPanelVacio3 = new JPanel();
 		JPanel pPanelVacio4 = new JPanel();
-		JButton btnRegistrarse = new JButton("Atrás");
+		JButton btnRegistrarse = new JButton("AtrÃ¡s");
 		
-		JLabel lContrasenyaActual = new JLabel("Contraseña actual:");
+		JLabel lContrasenyaActual = new JLabel("ContraseÃ±a actual:");
 		JPasswordField txtContrasenyaActual = new JPasswordField();
-		JLabel lErrorContrasenyaActual = new JLabel("La contraseña no es correcta.");
+		JLabel lErrorContrasenyaActual = new JLabel("La contraseÃ±a no es correcta.");
 		lErrorContrasenyaActual.setForeground(Color.RED);
 		lErrorContrasenyaActual.setVisible(false);
-		JLabel lContrasenyaNueva = new JLabel("Contraseña nueva:");
+		JLabel lContrasenyaNueva = new JLabel("ContraseÃ±a nueva:");
 		JPasswordField txtContrasenyaNueva = new JPasswordField();
 		JPanel pPanelVacio5 = new JPanel();
-		JLabel lContrasenyaRepetir = new JLabel("Repite la contraseña nueva:");
+		JLabel lContrasenyaRepetir = new JLabel("Repite la contraseÃ±a nueva:");
 		JPasswordField txtContrasenyaRepetir = new JPasswordField();
-		JLabel lErrorContrasenyaRepetir = new JLabel("La contraseña no coincide.");
+		JLabel lErrorContrasenyaRepetir = new JLabel("La contraseÃ±a no coincide.");
 		lErrorContrasenyaRepetir.setForeground(Color.RED);
 		lErrorContrasenyaRepetir.setVisible(false);
 		JButton btnAceptar = new JButton("Aceptar");
 		
-		// Asignación de los componentes a los contenedores.
+		// AsignaciÃ³n de los componentes a los contenedores.
 		pNorte.add(btnInicio);
 		pNorte.add(pNombreTienda);
 		pNorte.add(pPanelVacio1);
@@ -69,10 +74,42 @@ public class VentanaCambiarContrasenya extends JFrame {
 		add(pCentro, BorderLayout.CENTER);
 		add(pSur, BorderLayout.SOUTH);
 		
-		// Configuración de la ventana.
+		// ConfiguraciÃ³n de la ventana.
 		setVisible(true);
 		
-		// Configuración de los eventos.
+		// ConfiguraciÃ³n de los eventos.
+		
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtContrasenyaActual.getText() == u.getContrasenya()) {
+					if(txtContrasenyaNueva.getText().length() > 3 && txtContrasenyaNueva.getText().equals(txtContrasenyaRepetir)) {
+						u.setEmail(txtContrasenyaNueva.getText());
+						JOptionPane.showMessageDialog(null, "Contrasenya cambiada con exito");
+						VentanaAjustes va = new VentanaAjustes(u);
+						dispose();
+					} else {
+						JOptionPane.showMessageDialog(null, "La contrasenya no es valida o no coincide");
+					}
+				} else {
+					lErrorContrasenyaActual.show();
+				}
+			}
+		});
+		
+		btnRegistrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				VentanaAjustes va = new VentanaAjustes(u);
+			}
+		});
+		
+		btnInicio.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				VentanaUsuario vp = new VentanaUsuario(u);
+			}
+		});
 		
 	}
 
